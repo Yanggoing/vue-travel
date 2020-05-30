f23f896b334f36c2999e2b792a79b754d664fd8f
yang_going <2210744381@qq.com>
2020-05-30 17:12:02 +0800

    城市搜索

---
+++ src/components/City/City.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <city-header></city-header>
+    <city-search></city-search>
+  </div>
+</template>
+
+<script>
+import CityHeader from './Header'
+import CitySearch from './Search'
+export default {
+  name: 'City',
+  components: {
+    CityHeader,
+    CitySearch
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/City/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">
+    城市选择
+    <router-link to="/home">
+      <div class="header-back iconfont icon-arrow-left"></div>
+    </router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CityHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "../../assets/css/varibles.styl"
+  .header
+    position relative
+    height $headerHeight
+    overflow hidden
+    line-height $headerHeight
+    text-align center
+    font-size .32rem
+    color #fff
+    background $bgColor
+
+    .header-back
+      position absolute
+      top 0
+      left 0
+      width .64rem
+      text-align center
+      font-size .4rem
+      color #fff
+
+</style>
