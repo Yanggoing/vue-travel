cb9d7a366cb195c5ff3ec69ef0e14c1f84b827b2
yang_going <2210744381@qq.com>
2020-05-30 16:23:04 +0800

    axios数据请求 更改了数据

--- src/components/Home/Recommond.vue
+++ src/components/Home/Recommond.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="title">热门推荐</div>
     <ul>
-      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+      <li class="item border-bottom" v-for="item of list" :key="item.id">
         <img class="item-img"
              :src="item.imgUrl"
              alt="">
@@ -19,35 +19,8 @@
 <script>
 export default {
   name: 'HomeRecommond',
-  data () {
-    return {
-      recommendList: [{
-        id: '0001',
-        imgUrl: 'https://imgs.qunarzz.com/sight/p0/2005/a9/a9411416ff215fe2a3.water.jpg_200x200_3110fb7b.jpg',
-        title: '上海迪士尼乐园',
-        desc: '全球最大的迪士尼城堡'
-      }, {
-        id: '0002',
-        imgUrl: 'https://imgs.qunarzz.com/tuan/team2/1507/2c/83e0e0e7ae082a.jpg_200x200_6d7378b2.jpg',
-        title: '东方明珠',
-        desc: '259米透明悬空观光廊了解一下~'
-      }, {
-        id: '0003',
-        imgUrl: 'https://imgs.qunarzz.com/sight/p0/1811/7e/7ecbd353174c66b1a3.img.jpg_200x200_f661532a.jpg',
-        title: '上海欢乐谷',
-        desc: '敢上1200米长的木质过山车吗？'
-      }, {
-        id: '0004',
-        imgUrl: 'https://imgs.qunarzz.com/sight/p0/1512/b5/b5be74400df235aa90.water.jpg_200x200_192ddb7f.jpg',
-        title: '环球金融中心观光厅',
-        desc: '全球最大的迪士尼城堡'
-      }, {
-        id: '0005',
-        imgUrl: 'https://imgs.qunarzz.com/sight/p0/2005/a9/a9411416ff215fe2a3.water.jpg_200x200_3110fb7b.jpg',
-        title: '上海迪士尼乐园',
-        desc: '上海夜景尽收眼底'
-      }]
-    }
+  props: {
+    list: Array
   }
 }
 </script>
--- src/components/Home/Weekend.vue
+++ src/components/Home/Weekend.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="title">周末去哪儿</div>
     <ul>
-      <li class="item border-bottom" v-for="item of weekendList" :key="item.id">
+      <li class="item border-bottom" v-for="item of list" :key="item.id">
         <div class="item-img-wrapper">
           <img class="item-img"
                :src="item.imgUrl"
@@ -20,30 +20,8 @@
 <script>
 export default {
   name: 'HomeWeekend',
-  data () {
-    return {
-      weekendList: [{
-        id: '0001',
-        imgUrl: 'https://imgs.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg',
-        title: '上海必打卡',
-        desc: '中西合璧，现代和传统各有各的精彩'
-      }, {
-        id: '0002',
-        imgUrl: 'https://imgs.qunarzz.com/sight/source/1511/1d/fe098e8cfec117.jpg_r_640x214_a9be292a.jpg',
-        title: '上海城市观光',
-        desc: '来这里寻找你所不认识的魔都 ，感受云端漫步，品味老上海风情，享受休闲时光'
-      }, {
-        id: '0003',
-        imgUrl: 'https://imgs.qunarzz.com/sight/source/1505/b7/2f4621a3a4e12c.jpg_r_640x214_d55c10ba.jpg',
-        title: '学生最爱TOP10',
-        desc: '学习之余最爱玩耍，爱玩的你不可错过'
-      }, {
-        id: '0004',
-        imgUrl: 'https://imgs.qunarzz.com/sight/source/1505/7b/2a6b0ce087f0e5.jpg_r_640x214_5c76b115.jpg',
-        title: '情迷博物馆',
-        desc: '走进艺术殿堂，电影博物馆生动演绎着电影人和电影背后的故事，你不想知道吗~'
-      }]
-    }
+  props: {
+    list: Array
   }
 }
 </script>
@@ -51,7 +29,6 @@ export default {
 <style lang="stylus" scoped>
   @import '../../assets/css/mixins.styl'
   .title
-    margin-top .4rem
     line-height .8rem
     background #eee
     text-indent .2rem
